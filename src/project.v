ac97fc610618d1230123b02ecf68b390429192ef
Pigduck12 <eliot013@hotmail.com>
2025-09-25 16:57:16 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -22,6 +22,6 @@ module tt_um_uwasic_onboarding_eliot_tong (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, clk, rst_n, 1'b0, uio_in, ui_in[7:3]};
 
 endmodule
