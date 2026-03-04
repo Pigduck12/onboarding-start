f62050f2a32e6ca527710bdec65adcbf9d93cabe
Pigduck12 <eliot013@hotmail.com>
2026-03-03 22:59:49 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -9,11 +9,20 @@ module tt_um_uwasic_onboarding_eliot_tong (
     assign uio_oe = 8'hFF; // Set all IOs to output
   
   // Create wires to refer to the values of the registers
-  wire [7:0] en_reg_out_7_0;
-  wire [7:0] en_reg_out_15_8;
-  wire [7:0] en_reg_pwm_7_0;
-  wire [7:0] en_reg_pwm_15_8;
-  wire [7:0] pwm_duty_cycle;
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+    wire [7:0] en_reg_out_7_0;
+    wire [7:0] en_reg_out_15_8;
+    wire [7:0] en_reg_pwm_7_0;
+    wire [7:0] en_reg_pwm_15_8;
+    wire [7:0] pwm_duty_cycle;
+);
 pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
