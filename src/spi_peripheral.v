01bb559546342b4dee45dea82a44b0517c1a7871
Pigduck12 <eliot013@hotmail.com>
2026-03-04 09:57:08 -0500

    Update spi_peripheral.v

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -1,3 +1,14 @@
 module spi_peripheral (
-  
+  input wire CS_n,
+  input wire SS,
+  input wire COPI,
+  input wire SCLK,
+  output wire CIPO,
+  output reg[7:0] bitsTransferred,
+  output reg bitsCompleted
 );
+  reg[7:0] bitShifter;
+  reg[2:0] bitount;
+  always @(posedge SCLK or posedge CS_n)begin
+ 
+  end
