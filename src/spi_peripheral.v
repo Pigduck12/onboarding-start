91db78a04c584baa6a71eb2170250061ab3eb734
Pigduck12 <eliot013@hotmail.com>
2026-03-04 19:22:53 -0500

    Update spi_peripheral.v

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -3,29 +3,31 @@ module spi_peripheral (
   input wire SS,
   input wire COPI,
   input wire SCLK,
+  input wire clk,
+  input wire rst_n
   output wire CIPO,
   output reg[7:0] bitsTransferred,
-  output reg bitsCompleted
+  output reg bitCompleted
 );
   reg[7:0] bitShifter;
   reg[2:0] bitcount;
-  always @(posedge SCLK or posedge CS_n)begin
-    if (CS_n)begin
+  
+  always @(posedge SCLK or posedge CS_n)begin //this starts 
+    if (CS_n)begin //closed
       //reset values
-      bitsTransferred <= 8'b0;
-      bitsCompleted <= 1'b0;
+      bitCompleted <= 1'b0;
       bitShifter <= 8'b0;
       bitcount <= 3'b0;
-    end else begin
-      bitShifter <= {bitshifter[6:0],COPI};
-      bitcount <= bitcount + 1'b1;
-      if bitcount == 3'b111 begin
-        bitsCompleted = 1'b1;
-        bitsTransferred = 
-      end else begin
-        bitCompleted = 1'b0;
-        bitcount <= bitcount +1'b1;
-      
+    end else begin //close
+      bitShifter <= {bitShifter[6:0],COPI};
+      if (bitcount == 3'b111) begin //close
+        bitCompleted <= ~bitCompleted;
+        bitsTransferred <= {bitShifter[6:0],COPI}; 
+        bitcount <= 3'b0;
+      end else begin //close
+        bitCompleted <= 1'b0;
+        bitcount <= bitcount + 1'b1;
     end
+  end
   end
   end module
