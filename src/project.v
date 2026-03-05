b48d95d2c2de2eb1bc2ff5c5fb1df95998ae92c1
Pigduck12 <eliot013@hotmail.com>
2026-03-05 16:09:16 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -24,7 +24,9 @@ module tt_um_uwasic_onboarding_eliot_tong (
     wire [7:0] en_reg_pwm_7_0;
     wire [7:0] en_reg_pwm_15_8;
     wire [7:0] pwm_duty_cycle;
-  wire [15:0] pwm_raw_outputs;
+    wire [15:0] pwm_raw_outputs;
+    wire       w_pwm_signal;
+  
 pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
     .rst_n(rst_n),
@@ -32,9 +34,9 @@ pwm_peripheral pwm_peripheral_inst (
     .en_reg_out_15_8(en_reg_out_15_8),
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
-  .out(pwm_raw_outputs),
   .pwm_duty_cycle(pwm_duty_cycle),  // Use data from SPI
-  .spi_data_updated(spi_ready)
+  .spi_data_updated(spi_ready),
+  .pwm_out(w_pwm_signal)
   );
   spi_peripheral spi_peripheral_inst(
     .clk(clk),
@@ -43,20 +45,21 @@ pwm_peripheral pwm_peripheral_inst (
     .COPI(ui_in[1]),
     .CS_n(ui_in[2]),
     .CIPO(uo_out[0]),
+    .reg_uo_en(en_reg_out_7_0),           // Address 0x00
+    .reg_uio_en(en_reg_out_15_8),     // Address 0x01
+    .reg_pwm_uo_sel(en_reg_pwm_7_0),  // Address 0x02
+    .reg_pwm_uio_sel(en_reg_pwm_15_8),// Address 0x03
+    .reg_pwm_duty(pwm_duty_cycle),     // Address 0x04
     .bitsTransferred(spi_data), // Bridge to PWM
     .bitCompleted(spi_ready)
   ); //create what needs to go into spiperipheral
   // All output pins must be assigned. If not used, assign to 0.
   
   // Example: ou_out is the sum of ui_in and uio_in
-  assign pwm_duty_cycle = spi_data;
-  assign en_reg_out_7_0 = 8'hFF;   // Enable all outputs
-  assign en_reg_pwm_7_0 = 8'hFF;
-  assign en_reg_out_15_8 = 8'hFF; // All 8 bits set to 1 (Enabled)
-  assign en_reg_pwm_15_8 = 8'hFF; // All 8 bits set to 1 (PWM Mode)
-  assign uio_out = pwm_raw_outputs[15:8];
+  assign uio_out = (en_reg_pwm_15_8) ? pwm_raw_outputs[15:8] : en_reg_out_15_8;
   assign uio_oe  = 8'hFF;
-  assign uo_out[7:1]  = pwm_raw_outputs[7:1];
+  assign uo_out[7:1]  = (en_reg_pwm_7_0[7:1]) ? pwm_raw_outputs[7:1] : en_reg_out_7_0[7:1];
+  assign pwm_raw_outputs = {16{w_pwm_signal}};
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0,pwm_raw_outputs[0]};
