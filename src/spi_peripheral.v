4d43271f88ba83445fe86d242d69eff0663e9497
Pigduck12 <eliot013@hotmail.com>
2026-03-05 16:06:46 -0500

    Update spi_peripheral.v

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -6,20 +6,30 @@ module spi_peripheral (
   input wire rst_n,
   output wire CIPO,
   output reg[7:0] bitsTransferred,
-  output reg bitCompleted
+  output reg bitCompleted,
+  output reg [7:0] reg_uo_en,       // Address 0x00
+  output reg [7:0] reg_uio_en,      // Address 0x01
+  output reg [7:0] reg_pwm_uo_sel,  // Address 0x02
+  output reg [7:0] reg_pwm_uio_sel, // Address 0x03
+  output reg [7:0] reg_pwm_duty   // Address 0x04
 );
   reg[15:0] bitShifter;
   reg[3:0] bitcount;
   reg sclk_prev; // To store the previous state of SCLK
-  assign CIPO = bitShifter[7];
+  assign CIPO = bitShifter[15];
   always @(posedge clk or negedge rst_n)begin //this starts 
     if (!rst_n) begin
             // Total Hardware Reset
             bitcount     <= 4'b0;
-            bitShifter   <= 8'b0;
+            bitShifter   <= 16'b0;
             bitCompleted <= 1'b0;
             sclk_prev    <= 1'b0;
             bitsTransferred <= 8'b0; 
+            reg_uo_en       <= 8'h00;
+            reg_uio_en      <= 8'h00;
+            reg_pwm_uo_sel  <= 8'h00;
+            reg_pwm_uio_sel <= 8'h00;
+            reg_pwm_duty    <= 8'h00;
     end else begin
       sclk_prev <= SCLK;
     if (CS_n)begin //closed
@@ -30,7 +40,14 @@ module spi_peripheral (
         bitCompleted <= 1'b0;
         bitcount <= bitcount + 1'b1;
       if (bitcount == 4'd15) begin //close
-        case ()
+        case (bitShifter[14:8])
+            7'h00 : reg_uo_en <= {bitShifter[6:0],COPI}; 
+            7'h01 : reg_uio_en <= {bitShifter[6:0],COPI};
+            7'h02 : reg_pwm_uo_sel <= {bitShifter[6:0],COPI};
+            7'h03 : reg_pwm_uio_sel <= {bitShifter[6:0],COPI};
+            7'h04 : reg_pwm_duty <= {bitShifter[6:0],COPI};
+            default : ;
+        endcase
           
         bitCompleted <= 1'b1;
         bitsTransferred <= {bitShifter[6:0],COPI}; 
