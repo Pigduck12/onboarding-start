ab1141290492b3adad343fe49c11ac0f1b7233b2
Pigduck12 <eliot013@hotmail.com>
2026-03-04 20:31:27 -0500

    Update pwm_peripheral.v

--- src/pwm_peripheral.v
+++ src/pwm_peripheral.v
@@ -13,20 +13,33 @@ module pwm_peripheral (
     input  wire [7:0] en_reg_pwm_7_0,
     input  wire [7:0] en_reg_pwm_15_8,
     input  wire [7:0] pwm_duty_cycle,
+    input wire spi_data_updated,
     output reg [15:0] out
 );
 
     localparam clk_div_trig = 12; // Divide by (12+1)*256, yielding 3000 (3004.80769) Hz
     reg [10:0] clk_counter;
     reg [7:0] pwm_counter;
-    wire pwm_signal = (pwm_duty_cycle == 8'hFF) ? 1'b1 : (pwm_counter < pwm_duty_cycle); // 253 is 98.82% 254 is 99.21%, 255 is 100%, not 99.61%
+    reg        sync_0, sync_1, sync_2;
+    reg [7:0]  safe_duty_cycle;
+    wire pwm_signal = (safe_duty_cycle == 8'hFF) ? 1'b1 : (pwm_counter < safe_duty_cycle); // 253 is 98.82% 254 is 99.21%, 255 is 100%, not 99.61%
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             out <= 0;
             pwm_counter <= 0;
             clk_counter <= 0;
+            sync_0 <= 0;
+            sync_1 <= 0;
+            sync_2 <= 0;
+            safe_duty_cycle <= 0;
         end else begin
+            sync_0 <= spi_data_updated;
+            sync_1 <= sync_0;            
+            sync_2 <= sync_1;     
+            if (sync_1 && !sync_2) begin //what??
+                safe_duty_cycle <= pwm_duty_cycle; // Capture the data safely
+            end
             clk_counter <= clk_counter + 1;
             if (clk_counter == clk_div_trig) begin
                 pwm_counter <= pwm_counter + 1;
@@ -58,5 +71,6 @@ module pwm_peripheral (
     end
 
 endmodule
+
 
 
