70eea70babdfe9eb7732ce8757c4522dff5bafca
Pigduck12 <eliot013@hotmail.com>
2026-03-05 12:14:06 -0500

    Update spi_peripheral.v

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -8,14 +8,14 @@ module spi_peripheral (
   output reg[7:0] bitsTransferred,
   output reg bitCompleted
 );
-  reg[7:0] bitShifter;
-  reg[2:0] bitcount;
+  reg[15:0] bitShifter;
+  reg[3:0] bitcount;
   reg sclk_prev; // To store the previous state of SCLK
   assign CIPO = bitShifter[7];
   always @(posedge clk or negedge rst_n)begin //this starts 
     if (!rst_n) begin
             // Total Hardware Reset
-            bitcount     <= 3'b0;
+            bitcount     <= 4'b0;
             bitShifter   <= 8'b0;
             bitCompleted <= 1'b0;
             sclk_prev    <= 1'b0;
@@ -23,16 +23,16 @@ module spi_peripheral (
     end else begin
       sclk_prev <= SCLK;
     if (CS_n)begin //closed
-      bitcount     <= 3'b0;
+      bitcount     <= 4'b0;
       bitCompleted <= 1'b0;
     end else if (SCLK && !sclk_prev) begin 
-        bitShifter <= {bitShifter[6:0],COPI};
+      bitShifter <= {bitShifter[14:0],COPI};
         bitCompleted <= 1'b0;
         bitcount <= bitcount + 1'b1;
-      if (bitcount == 3'b111) begin //close
+      if (bitcount == 4'd15) begin //close
         bitCompleted <= 1'b1;
         bitsTransferred <= {bitShifter[6:0],COPI}; 
-        bitcount <= 3'b0;
+        bitcount <= 4'b0;
       end else begin //close
         bitCompleted <= 1'b0;
     end
