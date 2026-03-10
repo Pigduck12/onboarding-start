b5f0c1174710daf0bb51bcedd721eb1dd3ce2334
Pigduck12 <eliot013@hotmail.com>
2026-03-10 11:37:22 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -55,9 +55,9 @@ pwm_peripheral pwm_peripheral_inst (
   // All output pins must be assigned. If not used, assign to 0.
   
   // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_oe  = 8'hFF;
-  assign uo_out[0] = cipo_signal;
-  assign uo_out[7:1] = (pwm_raw_outputs[7:1] & en_reg_pwm_7_0[7:1]) | en_reg_out_7_0[7:1];
+  assign uio_oe  = 8'hFF;  
+  assign uo_out = (pwm_raw_outputs[7:0] & en_reg_pwm_7_0[7:0]) | en_reg_out_7_0[7:0]; 
+  assign uio_out[7] = cipo_signal;
   assign uio_out = (pwm_raw_outputs[15:8] & en_reg_pwm_15_8) | en_reg_out_15_8;
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0,pwm_raw_outputs[0],spi_data};
