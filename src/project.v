21716807d212a0546b047d0ec81d027734f53700
Pigduck12 <eliot013@hotmail.com>
2026-03-11 09:50:13 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -56,10 +56,10 @@ pwm_peripheral pwm_peripheral_inst (
   
   // Example: ou_out is the sum of ui_in and uio_in
   assign uio_oe  = 8'hFF;  
-  assign uo_out = (pwm_raw_outputs[7:0] & en_reg_pwm_7_0[7:0]) | en_reg_out_7_0[7:0]; 
+  assign uo_out = (pwm_raw_outputs[7:0] & en_reg_pwm_7_0) | en_reg_out_7_0; 
   assign uio_out[7] = cipo_signal;
-  assign uio_out = (pwm_raw_outputs[15:8] & en_reg_pwm_15_8) | en_reg_out_15_8;
+  assign uio_out[6:0] = (pwm_raw_outputs[15:8] & en_reg_pwm_15_8) | en_reg_out_15_8;
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0,pwm_raw_outputs[0],spi_data};
+  wire _unused = &{ena, ui_in[7:3], uio_in,pwm_raw_outputs[15],spi_data};
     
 endmodule
